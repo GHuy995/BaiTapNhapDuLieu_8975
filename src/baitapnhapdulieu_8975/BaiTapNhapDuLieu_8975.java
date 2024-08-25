4b95eb38f2524ad46dab56301055ae7c518280fb
Ghuy5 <huymai2k5tptdqb@gmail.com>
2024-08-25 07:10:59 +0700

    BaiTapNhapDuLieu8975

---
+++ src/baitapnhapdulieu_8975/BaiTapNhapDuLieu_8975.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baitapnhapdulieu_8975;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer
+ */
+public class BaiTapNhapDuLieu_8975 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap du lieu");
+        String name = scanner.nextLine();
+        System.out.println("name"+name);
+    }
+    
+}
